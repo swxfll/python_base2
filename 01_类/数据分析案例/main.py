e3adcd06d8c4da0cd4821bb2646374bbf6e51950
swxfll <swxfll@outlook.com>
2022-11-01 13:04:15 +0800

    完成数据分析案例

--- 01_类/数据分析案例/main.py
+++ 01_类/数据分析案例/main.py
@@ -7,3 +7,38 @@
 4. 进行数据需求的逻辑计算
 5. 通过PyEcharts进行图形绘制
 """
+
+from file_define import FileReader, TextFileReader, JsonFileReader
+from data_define import Record
+from pyecharts.charts import Bar
+from pyecharts.options import *
+from pyecharts.globals import ThemeType
+
+text_file_reader = TextFileReader("2011年1月销售数据.txt")
+text_file_date: list[Record] = text_file_reader.read_data()
+
+json_file_reader = JsonFileReader("2011年2月销售数据JSON.txt")
+json_file_data: list[Record] = json_file_reader.read_data()
+
+# 将2个月份的数据合并为1个list来存储
+all_data: list[Record] = text_file_date + json_file_data
+
+# 开始数据计算
+data_dict = {}
+for record in all_data:
+    if record.date in data_dict.keys():
+        # 当前日期已经有记录了, 所以和老记录做累加即可
+        data_dict[record.date] += record.money
+    else:
+        data_dict[record.date] = record.money
+
+# 可视化
+bar = Bar(init_opts=InitOpts(theme=ThemeType.LIGHT))
+
+bar.add_xaxis(list(data_dict.keys()))
+bar.add_yaxis("销售额", list(data_dict.values()), label_opts=LabelOpts(is_show=False))
+bar.set_global_opts(
+    title_opts=TitleOpts(title="每日销售额")
+)
+
+bar.render("每日销售额柱状图.html")
